6074c2ab85567b816e66e27e87c10b4154d6487e
Gregory Testo <gt@gregorytesto.com>
2022-08-19 18:47:37 +0200

    made changes for deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/remix-learneth-plugin'));
+app.use(express.static(__dirname + '/dist/remix-workshops-plugin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/remix-learneth-plugin/index.html'));
+res.sendFile(path.join(__dirname+'/dist/remix-workshops-plugin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
