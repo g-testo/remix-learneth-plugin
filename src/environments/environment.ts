04bd7652a44b915ac1376b2e1df96429c96ea980
Gregory Testo <gt@gregorytesto.com>
2022-08-20 07:48:00 +0200

    added custom github lesson

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   base: "/",
   apiUrl: `https://static.220.14.12.49.clients.your-server.de:3000/`,
   //apiUrl: `http://localhost:3001/`,
-  github:[{name:`ethereum/remix-workshops`,branch:`master`},{name:`GrandSchtroumpf/solidity-school`,branch:`master`}],
+  github:[{name:`g-testo/remix-workshops`,branch:`master`},{name:`ethereum/remix-workshops`,branch:`master`},{name:`GrandSchtroumpf/solidity-school`,branch:`master`}],
   help: 'https://github.com/bunsenstraat/remix-workshop-plugin/blob/master/README.md'
 };
